fec0bf7c6d71693c117dbf072faaf50e82e49a7f
Luz Frias <luzfrias@gmail.com>
2017-01-03 13:35:42 +0100

    Use barcelona biking example instead of Paris

--- 04_visualizacion/src/03_ggplot2_maps.R
+++ 04_visualizacion/src/03_ggplot2_maps.R
@@ -53,21 +53,21 @@ library(ggmap)
 
 # ggmap llama a la API de Google Maps para recuperar la imagen estática del mapa
 
-# Geolocalizo Paris
-paris <- geocode("paris")
+# Geolocalizo Barcelona
+bcn <- geocode("eixample, barcelona")
 
 # Descargo el mapa desde Google Maps
-map.paris <- get_map( location = as.numeric(paris),
-                       color = "color",
-                       maptype = "roadmap",
-                       zoom = 13)
+map.bcn <- get_map( location = as.numeric(bcn),
+                    color = "color",
+                    maptype = "roadmap",
+                    zoom = 13)
 
 # Lo pinto
-ggmap(map.paris)
+ggmap(map.bcn)
 
 # Vamos a pintar algo interesante. Esto ya sabemos hacerlo
 library(httr)
-res <- GET("https://api.citybik.es/v2/networks/velib")
+res <- GET("https://api.citybik.es/v2/networks/bicing")
 stop_for_status(res)
 info <- content(res)
 
@@ -78,21 +78,25 @@ parsed <- lapply(info$network$stations, function(s) c(lat = s$latitude,
                                                       free_bikes = s$free_bikes,
                                                       empty_slots = s$empty_slots))
 
-velib <- data.frame(matrix(unlist(parsed), ncol = 5, byrow = TRUE), stringsAsFactors = FALSE)
-names(velib) <- c("lat", "lon", "name", "free_bikes", "empty_slots")
+bicing <- data.frame(matrix(unlist(parsed), ncol = 5, byrow = TRUE), stringsAsFactors = FALSE)
+names(bicing) <- c("lat", "lon", "name", "free_bikes", "empty_slots")
 
 # Aplicamos las conversiones de tipo
-velib$lat         <- as.numeric(velib$lat)
-velib$lon         <- as.numeric(velib$lon)
-velib$empty_slots <- as.numeric(velib$empty_slots)
-velib$free_bikes  <- as.numeric(velib$free_bikes)
+bicing$lat         <- as.numeric(bicing$lat)
+bicing$lon         <- as.numeric(bicing$lon)
+bicing$empty_slots <- as.numeric(bicing$empty_slots)
+bicing$free_bikes  <- as.numeric(bicing$free_bikes)
 
 # Calculamos % disponible
 # Ojo, la división por cero provoca valores NaN
-velib$pct_free_bikes  <- velib$free_bikes  / (velib$free_bikes + velib$empty_slots)
-velib$pct_empty_slots <- velib$empty_slots / (velib$free_bikes + velib$empty_slots)
-velib[is.nan(velib$pct_free_bikes),  ]$pct_free_bikes  <- 0
-velib[is.nan(velib$pct_empty_slots), ]$pct_empty_slots <- 0
+bicing$pct_free_bikes  <- bicing$free_bikes  / (bicing$free_bikes + bicing$empty_slots)
+bicing$pct_empty_slots <- bicing$empty_slots / (bicing$free_bikes + bicing$empty_slots)
+if (length(which(is.nan(bicing$pct_free_bikes)))) {
+  bicing[is.nan(bicing$pct_free_bikes),  ]$pct_free_bikes  <- 0
+}
+if (length(which(is.nan(bicing$pct_empty_slots)))) {
+  bicing[is.nan(bicing$pct_empty_slots), ]$pct_empty_slots <- 0
+}
 
 # Asignamos colores
 get_availability <- function(pct) {
@@ -100,16 +104,16 @@ get_availability <- function(pct) {
   else if (pct < 0.30) "yellow"
   else                 "green"
 }
-velib$pick_availability <- sapply(velib$pct_free_bikes,  get_availability)
-velib$drop_availability <- sapply(velib$pct_empty_slots, get_availability)
+bicing$pick_availability <- sapply(bicing$pct_free_bikes,  get_availability)
+bicing$drop_availability <- sapply(bicing$pct_empty_slots, get_availability)
 
 # Por si tenemos que guardar / recuperar los datos limpios
-# write.table(velib, file = "dat/velib.tsv", row.names = FALSE, col.names = TRUE, sep = "\t")
-# velib <- read.table("dat/velib.tsv", header = TRUE, sep = "\t")
+# write.table(bicing, file = "dat/bicing.tsv", row.names = FALSE, col.names = TRUE, sep = "\t")
+# bicing <- read.table("dat/bicing.tsv", header = TRUE, sep = "\t")
 
 # Lo pinto, añadiendo mis puntos
-ggmap(map.paris) + 
-  geom_point(aes(x = lon, y = lat, color = pick_availability), data = velib) +
+ggmap(map.bcn) + 
+  geom_point(aes(x = lon, y = lat, color = drop_availability), data = bicing) +
   # ggmap asigna un color por defecto a cada valor del parámetro "color"
   # para mapear a algo que tenga sentido
   scale_colour_manual(values = c("green" = "green",
